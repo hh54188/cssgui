edd1379319dae16854b9f98de3321ef85bcae2fb
liguangyi <252438823@qq.com>
2022-05-05 00:28:29 +0800

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return <div className="footer">
-        Created by <a href="https://twitter.com/home" target="_blank">Guangyi Li</a>
+        Created by <a href="https://twitter.com/li_guangyi" target="_blank">Guangyi Li</a> / <a href="mailto:liguangyi08@gmail.com">Email Me</a>  
     </div>
 }
 
