d1683a3765be4dc1e62b310ab8c40cb9fe22eacf
liguangyi <252438823@qq.com>
2022-10-03 23:59:53 +0800

    add source code link to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 function Footer() {
     return <div className="footer">
-        Created by <a href="https://twitter.com/li_guangyi" target="_blank">Guangyi Li</a> / <a href="mailto:liguangyi08@gmail.com">Email Me</a>  
+        Created by <a href="https://twitter.com/li_guangyi" target="_blank">Guangyi Li</a> / <a href="mailto:liguangyi08@gmail.com">Email Me</a>  / <a href="https://github.com/hh54188/cssgui" target="_blank">Source Code</a>
     </div>
 }
 
